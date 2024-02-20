df80f5429d69483ec6d4b95abe287771f64c8b59
RAJI-Zakaria <zakariaraji.me@gmail.com>
2024-02-20 23:45:00 +0100

    fixing gradle dependencies for release and icon setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.bluetoothsample"
+    namespace = "com.example.redeemers_faz"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.bluetoothsample"
+        applicationId = "com.example.redeemers_faz"
         minSdk = 28
         targetSdk = 34
         versionCode = 1
@@ -21,50 +21,42 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     buildFeatures {
         compose = true
     }
+
 }
-
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.material3:material3:1.1.2")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
 
+    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.20.0")
 
     implementation("androidx.compose.ui:ui:1.4.1")
-    implementation("androidx.compose.material:material:1.4.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.4.1")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
-    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
     debugImplementation("androidx.compose.ui:ui-tooling:1.4.1")
-
 
     implementation("br.com.devsrsouza.compose.icons:feather:1.1.0")
 
-    implementation("androidx.compose.ui:ui:1.5.4")
-    implementation("androidx.compose.ui:ui-graphics:1.5.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
-    implementation("androidx.compose.material:material-icons-core:1.5.4")
-    implementation("androidx.compose.material:material-icons-extended:1.5.4")
-    implementation("androidx.compose.ui:ui:1.0.0")
-    implementation("androidx.compose.material:material:1.0.0")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha01")
-    implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
+    implementation("androidx.room:room-runtime:2.5.0")
+    kapt("androidx.room:room-compiler:2.5.0")
+    implementation("androidx.room:room-ktx:2.6.1")
+
     implementation("com.google.firebase:firebase-inappmessaging:20.4.0")
 
-    implementation("androidx.room:room-runtime:2.5.0")
-    annotationProcessor("androidx.room:room-compiler:2.5.0")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.5.0")
+    implementation("androidx.fragment:fragment:1.3.6")
 
+
+
+    implementation("androidx.compose.material:material-icons-core:1.5.4")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
+    implementation("androidx.compose.material:material:1.0.0")
+    implementation("androidx.compose.material3:material3:1.1.2")
 
 }
